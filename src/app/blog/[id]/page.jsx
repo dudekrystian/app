ed34cf102f149594e07041651151d52caba784c5
dudekrystian <dudekrystian@gmail.com>
2023-10-18 20:37:45 +0200

    add basic structure to blog page with style

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
 
- const Post = () => {
+
+
+
+const BlogPost =  () => {
   return (
-    <div>post</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>Title</h1>
+          <p className={styles.desc}>
+          </p>
+          <div className={styles.author}>
+            <Image
+              src={""}
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>{"data.username"}</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src={"https://images.pexels.com/photos/1569002/pexels-photo-1569002.jpeg?auto=compress&cs=tinysrgb&w=1600"}
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>
+         {"data.content"}
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Post;
+export default BlogPost;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
 import styles from './page.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
 
 
 const Blog = props => {
   return (
-    <div>Blog</div>
+    <div className={styles.mainContainer}>
+      <Link href="/blog/testId" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+          src="https://images.pexels.com/photos/5490803/pexels-photo-5490803.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+          alt=""
+          width={400}
+          height={250}
+          className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>desciption</p>
+        </div>
+      </Link>
+      <Link href="/testId" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+          src="https://images.pexels.com/photos/5490803/pexels-photo-5490803.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+          alt=""
+          width={400}
+          height={250}
+          className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>desciption</p>
+        </div>
+      </Link>
+    </div>
   )
 }
 
